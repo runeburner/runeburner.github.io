c6e3a20edfde0cdd7b7398f9215cfcf6ff2d7a6b
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-02-14 12:37:50 -0500

    Prevent printing

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,19 @@ import { IncantationsPage } from "./IncantationsPage";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { WorldPage } from "./WorldPage";
 import classes from "./App.module.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const f = (e: KeyboardEvent) => {
+      const isMeta = e.ctrlKey || e.metaKey;
+      if (isMeta && ["p"].includes(e.key)) {
+        e.preventDefault();
+      }
+    };
+    document.addEventListener("keydown", f);
+    return () => document.removeEventListener("keydown", f);
+  }, []);
   return (
     <div className={classes.fullscreenContainer}>
       <Sidebar />
