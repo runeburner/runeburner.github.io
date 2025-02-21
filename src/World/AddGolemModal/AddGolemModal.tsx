fb8d23f41c84bdda40862ad85512f9fa6715f187
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-02-21 17:56:36 -0500

    render v2

--- src/World/AddGolemModal/AddGolemModal.tsx
+++ src/World/AddGolemModal/AddGolemModal.tsx
@@ -5,6 +5,50 @@ import { RuneSlider } from "./RuneSlider/RuneSlider";
 import { Rune } from "../../Game/runes";
 import { Golem } from "../Golem/Golem";
 
+type DemoBarProps = {
+  label: string;
+  value: [number, number];
+  setValue: React.Dispatch<React.SetStateAction<[number, number]>>;
+};
+
+const DemoBar = ({ label, value, setValue }: DemoBarProps) => {
+  return (
+    <>
+      <span>{label}</span>
+      <input
+        type="range"
+        min={0}
+        max={50}
+        value={value[0]}
+        onChange={(e) =>
+          setValue(([, m]) => {
+            const nextCurrent = parseInt(e.target.value);
+            return [nextCurrent, Math.max(nextCurrent, m)];
+          })
+        }
+      />
+      <span>{value[0]}</span>
+      <br />
+
+      <span>max {label}</span>
+      <input
+        type="range"
+        min={0}
+        max={50}
+        value={value[1]}
+        onChange={(e) =>
+          setValue(([c]) => {
+            const nextMax = parseInt(e.target.value);
+            return [Math.min(nextMax, c), nextMax];
+          })
+        }
+      />
+      <span>{value[1]}</span>
+      <br />
+    </>
+  );
+};
+
 export const AddGolemModal = ({ open, onClose }: ModalProps) => {
   const [runes, setRunes] = useState<Record<Rune, number>>(
     Object.fromEntries(Object.values(Rune).map((r) => [r, 0])) as Record<
@@ -13,10 +57,9 @@ export const AddGolemModal = ({ open, onClose }: ModalProps) => {
     >
   );
 
-  const [health, setHealth] = useState(10);
-  const [armor, setArmor] = useState(10);
-  const [shield, setShield] = useState(10);
-  const [percent, setPercent] = useState(75);
+  const [health, setHealth] = useState<[number, number]>([11, 12]);
+  const [armor, setArmor] = useState<[number, number]>([0, 0]);
+  const [shield, setShield] = useState<[number, number]>([0, 0]);
 
   const appliedRunes: [Rune, number][] = Object.entries(runes).filter(
     (r) => r[1] > 0
@@ -31,7 +74,6 @@ export const AddGolemModal = ({ open, onClose }: ModalProps) => {
           health={health}
           armor={armor}
           shield={shield}
-          percent={percent / 100}
         />
         <p>Runes: {totalRunes}/6</p>
         <p>Rune Types: {appliedRunes.length}/3</p>
@@ -53,45 +95,9 @@ export const AddGolemModal = ({ open, onClose }: ModalProps) => {
           icon={OthalanIcon}
           amount={runes[Rune.VOID]}
         />
-        <span>health</span>
-        <input
-          type="range"
-          min={0}
-          max={50}
-          value={health}
-          onChange={(e) => setHealth(parseInt(e.target.value))}
-        />
-        <span>{health}</span>
-        <br></br>
-        <span>armor</span>
-        <input
-          type="range"
-          min={0}
-          max={50}
-          value={armor}
-          onChange={(e) => setArmor(parseInt(e.target.value))}
-        />
-        <span>{armor}</span>
-        <br></br>
-        <span>shield</span>
-        <input
-          type="range"
-          min={0}
-          max={50}
-          value={shield}
-          onChange={(e) => setShield(parseInt(e.target.value))}
-        />
-        <span>{shield}</span>
-        <br></br>
-        <span>percent</span>
-        <input
-          type="range"
-          min={0}
-          max={100}
-          value={percent}
-          onChange={(e) => setPercent(parseInt(e.target.value))}
-        />
-        <span>{percent}</span>
+        <DemoBar label="health" value={health} setValue={setHealth} />
+        <DemoBar label="armor" value={armor} setValue={setArmor} />
+        <DemoBar label="shield" value={shield} setValue={setShield} />
       </div>
     </Modal>
   );
--- src/World/Golem/Health/Health.tsx
+++ src/World/Golem/Health/Health.tsx
@@ -1,81 +1,142 @@
 const healthColor = "#fd4c63";
 const armorColor = "#fde86f";
 const shieldColor = "#65c4fb";
+const healthColors = [healthColor, armorColor, shieldColor, "#ffffff22"];
 
 const outerDiameter = 31;
 const innerDiameter = 20;
 
+const emptyHealth = (
+  <path d={`M0 32 A32 32 0 1 1 0.001 32L0.001 20 A20 20 0 1 0 0 20`} />
+);
+
 // spacing between chunks
 const spacing = Math.PI / 50;
 const innerSpacing = spacing * (outerDiameter / innerDiameter);
-const chunkHealthValue = 5;
+const chunkSize = 5;
 
-const getColor = (props: HealthProps, i: number): string => {
-  const c = i * chunkHealthValue;
-  if (c < props.health) return healthColor;
-  if (c < props.health + props.armor) return armorColor;
-  return shieldColor;
-};
+// return [health, armor, shield, empty] for up to chunkSize points
+const getNextPoints = (
+  props: HealthProps,
+  pointsUsed: number,
+  chunkSize: number
+): [number, number, number, number] => {
+  let pointsLeft = chunkSize;
+  const health = Math.min(
+    pointsLeft,
+    Math.max(0, props.health[0] - pointsUsed)
+  );
+  pointsLeft = Math.max(pointsLeft - health);
 
-const getOpacity = (props: HealthProps, i: number): [number, boolean] => {
-  const currentHealth = Math.floor(props.percent * props.totalHealth);
-  const min = i * chunkHealthValue;
-  const max = Math.min((i + 1) * chunkHealthValue, props.totalHealth);
-  console.log(min, max, currentHealth);
-  if (currentHealth >= max) return [1, false];
-  if (currentHealth < min) return [0.2, false];
-  return [((currentHealth - min) / (max - currentHealth)) * 0.8 + 0.2, true];
+  const armor = Math.min(
+    pointsLeft,
+    Math.min(
+      Math.max(0, props.health[0] + props.armor[0] - pointsUsed),
+      props.armor[0]
+    )
+  );
+  pointsLeft = Math.max(pointsLeft - armor);
+
+  const shield = Math.min(
+    pointsLeft,
+    Math.min(
+      Math.max(
+        0,
+        props.health[0] + props.armor[0] + props.shield[0] - pointsUsed
+      ),
+      props.shield[0]
+    )
+  );
+  pointsLeft = Math.max(pointsLeft - shield);
+  return [health, armor, shield, pointsLeft];
 };
 
 type HealthProps = {
-  health: number;
-  armor: number;
-  shield: number;
-  totalHealth: number;
-  percent: number;
+  health: [number, number];
+  armor: [number, number];
+  shield: [number, number];
 };
 export const Health = (props: HealthProps): React.ReactElement => {
-  const { totalHealth } = props;
-  const chunks = Math.ceil(totalHealth / chunkHealthValue);
+  const { health, armor, shield } = props;
+  const totalHealth = health[1] + armor[1] + shield[1];
+  if (totalHealth === 0) return <>{emptyHealth}</>;
   const paths: JSX.Element[] = [];
-  const radPerHealth = (Math.PI * 2 - spacing * chunks) / totalHealth;
-  const innerRadPerHealth = (Math.PI * 2 - innerSpacing * chunks) / totalHealth;
-  for (let i = 0; i < chunks; i++) {
-    const thisChunkValue =
-      i < chunks - 1 ? chunkHealthValue : totalHealth % (i * chunkHealthValue);
 
-    const startRad = i * (spacing + chunkHealthValue * radPerHealth);
-    const endRad = startRad + thisChunkValue * radPerHealth;
-    const p0 = [Math.sin(startRad), Math.cos(startRad)];
-    const p1 = [Math.sin(endRad), Math.cos(endRad)];
+  const numChunks = Math.ceil(totalHealth / chunkSize);
+  const outerRadPerPoint = (Math.PI * 2 - spacing * numChunks) / totalHealth;
+  const innerRadPerPoint =
+    (Math.PI * 2 - innerSpacing * numChunks) / totalHealth;
 
-    const innerStartRad =
-      i * (innerSpacing + chunkHealthValue * innerRadPerHealth);
-    const innerEndRad = innerStartRad + thisChunkValue * innerRadPerHealth;
-    const p3 = [Math.sin(innerStartRad), Math.cos(innerStartRad)];
-    const p4 = [Math.sin(innerEndRad), Math.cos(innerEndRad)];
-    const large = endRad - startRad > Math.PI ? 1 : 0;
+  let pointsUsed = 0;
+  for (let i = 0; i < numChunks; i++) {
+    const thisChunkSize =
+      i < numChunks - 1 ? chunkSize : totalHealth % (i * chunkSize);
+    const pts = getNextPoints(props, pointsUsed, thisChunkSize);
+    console.log(pts);
+    pointsUsed += pts.reduce((acc, c) => acc + c, 0);
 
-    const [op, selected] = getOpacity(props, i);
+    let startRad = i * (spacing + chunkSize * outerRadPerPoint);
+    let innerStartRad = i * (innerSpacing + chunkSize * innerRadPerPoint);
+    for (let j = 0; j < pts.length; j++) {
+      if (pts[j] === 0) continue;
+      const endRad = startRad + pts[j] * outerRadPerPoint;
+      const p0 = [Math.sin(startRad), Math.cos(startRad)];
+      const p1 = [Math.sin(endRad), Math.cos(endRad)];
 
-    paths.push(
-      <path
-        key={i}
-        opacity={op}
-        strokeWidth={selected ? 1 : 0}
-        stroke={selected ? "#fff" : ""}
-        fill={getColor(props, i)}
-        d={`M${p0[0] * outerDiameter} ${
-          p0[1] * outerDiameter
-        } A${outerDiameter} ${outerDiameter} 0 ${large} 0 ${
-          p1[0] * outerDiameter
-        } ${p1[1] * outerDiameter}L${p4[0] * innerDiameter} ${
-          p4[1] * innerDiameter
-        }A${innerDiameter} ${innerDiameter} 0 ${large} 1 ${
-          p3[0] * innerDiameter
-        } ${p3[1] * innerDiameter}Z`}
-      />
-    );
+      const innerEndRad = innerStartRad + pts[j] * innerRadPerPoint;
+      const p2 = [Math.sin(innerStartRad), Math.cos(innerStartRad)];
+      const p3 = [Math.sin(innerEndRad), Math.cos(innerEndRad)];
+      const large = endRad - startRad > Math.PI ? 1 : 0;
+
+      paths.push(
+        <path
+          key={`${i}_${j}`}
+          fill={healthColors[j]}
+          d={`M${p0[0] * outerDiameter} ${
+            p0[1] * outerDiameter
+          } A${outerDiameter} ${outerDiameter} 0 ${large} 0 ${
+            p1[0] * outerDiameter
+          } ${p1[1] * outerDiameter}L${p3[0] * innerDiameter} ${
+            p3[1] * innerDiameter
+          }A${innerDiameter} ${innerDiameter} 0 ${large} 1 ${
+            p2[0] * innerDiameter
+          } ${p2[1] * innerDiameter}Z`}
+        />
+      );
+      startRad = endRad;
+      innerStartRad = innerEndRad;
+    }
+    // let startRad = chunkStartRad;
+    // let endRad = chunkEndRad;
+    // const p0 = [Math.sin(startRad), Math.cos(startRad)];
+    // const p1 = [Math.sin(endRad), Math.cos(endRad)];
+
+    // const innerStartRad = i * (innerSpacing + chunkSize * innerRadPerPoint);
+    // const innerEndRad = innerStartRad + thisChunkSize * innerRadPerPoint;
+    // const p2 = [Math.sin(innerStartRad), Math.cos(innerStartRad)];
+    // const p3 = [Math.sin(innerEndRad), Math.cos(innerEndRad)];
+    // const large = endRad - startRad > Math.PI ? 1 : 0;
+
+    // const [op, selected] = getOpacity(totalHealth, i);
+
+    // paths.push(
+    //   <path
+    //     key={i}
+    //     opacity={op}
+    //     strokeWidth={selected ? 1 : 0}
+    //     stroke={selected ? "#fff" : ""}
+    //     fill={getColor()}
+    //     d={`M${p0[0] * outerDiameter} ${
+    //       p0[1] * outerDiameter
+    //     } A${outerDiameter} ${outerDiameter} 0 ${large} 0 ${
+    //       p1[0] * outerDiameter
+    //     } ${p1[1] * outerDiameter}L${p3[0] * innerDiameter} ${
+    //       p3[1] * innerDiameter
+    //     }A${innerDiameter} ${innerDiameter} 0 ${large} 1 ${
+    //       p2[0] * innerDiameter
+    //     } ${p2[1] * innerDiameter}Z`}
+    //   />
+    // );
   }
   return <>{paths}</>;
 };
