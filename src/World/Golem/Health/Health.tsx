fec8544d860d04e234a946d51c4ac3483dbd7e4e
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-02-22 08:21:46 -0500

    Remove health debug from add golem

--- src/World/Golem/Health/Health.tsx
+++ src/World/Golem/Health/Health.tsx
@@ -1,17 +1,21 @@
 const healthColor = "#fd4c63";
 const armorColor = "#fde86f";
 const shieldColor = "#65c4fb";
-const healthColors = [healthColor, armorColor, shieldColor, "#ffffff22"];
+const emptyColor = "#ffffff22";
+const healthColors = [healthColor, armorColor, shieldColor];
 
 const outerDiameter = 31;
 const innerDiameter = 20;
 
 const emptyHealth = (
-  <path d={`M0 32 A32 32 0 1 1 0.001 32L0.001 20 A20 20 0 1 0 0 20`} />
+  <path
+    fill={emptyColor}
+    d={`M0 32 A32 32 0 1 1 0.001 32L0.001 20 A20 20 0 1 0 0 20`}
+  />
 );
 
 // spacing between chunks
-const spacing = Math.PI / 50;
+const spacing = Math.PI / 32;
 const innerSpacing = spacing * (outerDiameter / innerDiameter);
 const chunkSize = 5;
 
@@ -72,7 +76,6 @@ export const Health = (props: HealthProps): React.ReactElement => {
     const thisChunkSize =
       i < numChunks - 1 ? chunkSize : totalHealth % (i * chunkSize);
     const pts = getNextPoints(props, pointsUsed, thisChunkSize);
-    console.log(pts);
     pointsUsed += pts.reduce((acc, c) => acc + c, 0);
 
     let startRad = i * (spacing + chunkSize * outerRadPerPoint);
@@ -106,37 +109,6 @@ export const Health = (props: HealthProps): React.ReactElement => {
       startRad = endRad;
       innerStartRad = innerEndRad;
     }
-    // let startRad = chunkStartRad;
-    // let endRad = chunkEndRad;
-    // const p0 = [Math.sin(startRad), Math.cos(startRad)];
-    // const p1 = [Math.sin(endRad), Math.cos(endRad)];
-
-    // const innerStartRad = i * (innerSpacing + chunkSize * innerRadPerPoint);
-    // const innerEndRad = innerStartRad + thisChunkSize * innerRadPerPoint;
-    // const p2 = [Math.sin(innerStartRad), Math.cos(innerStartRad)];
-    // const p3 = [Math.sin(innerEndRad), Math.cos(innerEndRad)];
-    // const large = endRad - startRad > Math.PI ? 1 : 0;
-
-    // const [op, selected] = getOpacity(totalHealth, i);
-
-    // paths.push(
-    //   <path
-    //     key={i}
-    //     opacity={op}
-    //     strokeWidth={selected ? 1 : 0}
-    //     stroke={selected ? "#fff" : ""}
-    //     fill={getColor()}
-    //     d={`M${p0[0] * outerDiameter} ${
-    //       p0[1] * outerDiameter
-    //     } A${outerDiameter} ${outerDiameter} 0 ${large} 0 ${
-    //       p1[0] * outerDiameter
-    //     } ${p1[1] * outerDiameter}L${p3[0] * innerDiameter} ${
-    //       p3[1] * innerDiameter
-    //     }A${innerDiameter} ${innerDiameter} 0 ${large} 1 ${
-    //       p2[0] * innerDiameter
-    //     } ${p2[1] * innerDiameter}Z`}
-    //   />
-    // );
   }
   return <>{paths}</>;
 };
