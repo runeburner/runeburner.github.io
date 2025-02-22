ee6b8bb9e5e6bda561d66a80a255ce101404e898
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-02-22 09:52:15 -0500

    convert map to Int32Array

--- src/Game/worker.ts
+++ src/Game/worker.ts
@@ -1,7 +1,7 @@
 import { Entity } from "./Entity";
 import "./channel";
 
-export const map: number[][] = [
+export const defaultMap: number[][] = [
   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5],
   [8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3],
   [6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1],
@@ -19,6 +19,45 @@ export const map: number[][] = [
   [8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3],
   [6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1],
 ];
+
+// export const defaultMap: number[][] = [
+//   [1, 2, 3, 4, 5, 6],
+//   [7, 8, 9, 0, 1, 2],
+//   [3, 4, 5, 6, 7, 8],
+//   [9, 0, 1, 2, 3, 4],
+//   [5, 6, 7, 8, 9, 0],
+//   [1, 2, 3, 4, 5, 6],
+// ];
+// export const defaultMap: number[][] = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 1, 2, 3],
+// ];
+
+// First int is the tile id, second is the metadata
+export const ValuesPerTile = 2;
+
+export type Map = {
+  width: number;
+  height: number;
+  data: Int32Array;
+};
+
+export const map: Map = (() => {
+  const height = defaultMap.length;
+  const width = defaultMap[0].length;
+  const data = new Int32Array(width * height * ValuesPerTile);
+  for (let j = 0; j < height; j++) {
+    for (let i = 0; i < width; i++) {
+      data[(j * width + i) * ValuesPerTile] = defaultMap[j][i];
+    }
+  }
+  return {
+    width,
+    height,
+    data,
+  };
+})();
 
 export const entities: Entity[] = [];
 
