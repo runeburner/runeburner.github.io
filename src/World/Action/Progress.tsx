fc13c94bbe794d2bbe8e8b48f2fd8b8abe21db5f
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-03-17 23:20:10 -0400

    More progress

--- src/World/Action/Progress.tsx
+++ src/World/Action/Progress.tsx
@@ -1,8 +1,6 @@
-import { useEffect, useRef, useState } from "react";
-import { ActionProgress } from "../../types/actions";
 import { game } from "../../Game/game";
 import { eq } from "../../types/vec";
-import { uiUpdate, useUIThrottle } from "../uiThrottler";
+import { makeThrottledUse } from "../uiThrottler";
 
 // const useEntityPos = (id: number): Vec => {
 //   const ref = useRef<Vec>(game.entityM[id].pos);
@@ -24,58 +22,38 @@ import { uiUpdate, useUIThrottle } from "../uiThrottler";
 //   return pos;
 // };
 
-export const actionUpdateMap: Record<string, (a: ActionProgress) => void> = {};
+export const [actionUpdateMap, useAction] = makeThrottledUse(
+  (id) => game.actionM[id]
+);
 
-export const useAction = (id: number): ActionProgress => {
-  const ref = useRef<ActionProgress>(game.actionM[id]);
-  const [action, setAction] = useState<ActionProgress>(game.actionM[id]);
-  useUIThrottle(() => {
-    if (ref.current === action) return;
-    setAction(ref.current);
-  });
+// export const actionUpdateMap: Record<string, (a: ActionProgress) => void> = {};
 
-  useEffect(() => {
-    actionUpdateMap[id] = (p) => {
-      ref.current = p;
-      uiUpdate();
-    };
-    return () => {
-      delete actionUpdateMap[id];
-    };
-  }, [id]);
-  return action;
-};
+// export const useAction = (id: number): ActionProgress => {
+//   const ref = useRef<ActionProgress>(game.actionM[id]);
+//   const [action, setAction] = useState<ActionProgress>(game.actionM[id]);
+//   useUIThrottle(() => {
+//     if (ref.current === action) return;
+//     setAction(ref.current);
+//   });
 
-export const progressUpdateMap: Record<string, (v: Vec) => void> = {};
+//   useEffect(() => {
+//     actionUpdateMap[id] = (p) => {
+//       ref.current = p;
+//       uiUpdate();
+//     };
+//     return () => {
+//       delete actionUpdateMap[id];
+//     };
+//   }, [id]);
+//   return action;
+// };
 
-export const useActionProgress = (id: number): Vec => {
-  const [action, setAction] = useState<Vec>([
-    ...(game.actionM[id]?.progress ?? [0, 0]),
-  ]);
-  useEffect(() => {
-    progressUpdateMap[id] = (v): void => {
-      if (eq(v, action)) return;
-      setAction([...v]);
-    };
-    return () => {
-      delete progressUpdateMap[id];
-    };
-  }, [id]);
-  return action;
-};
+export const [progressUpdateMap, useActionProgress] = makeThrottledUse(
+  (id) => game.actionM[id]?.progress ?? [0, 0],
+  eq
+);
 
-export const progressPosUpdateMap: Record<string, (v: Vec) => void> = {};
-
-export const useActionProgressPos = (id: number): Vec => {
-  const [action, setAction] = useState<Vec>(game.actionM[id]?.pos ?? [0, 0]);
-  useEffect(() => {
-    progressPosUpdateMap[id] = (v): void => {
-      if (eq(v, action)) return;
-      setAction(v);
-    };
-    return (): void => {
-      delete progressPosUpdateMap[id];
-    };
-  }, [id]);
-  return action;
-};
+export const [progressPosUpdateMap, useActionProgressPos] = makeThrottledUse(
+  (id) => game.actionM[id]?.pos ?? [0, 0],
+  eq
+);
