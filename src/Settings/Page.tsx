bfd5eae4cd0b21f173d2dd5cc84b317cf53f1f6a
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-03-18 00:29:52 -0400

    add slider for ui fps

--- src/Settings/Page.tsx
+++ src/Settings/Page.tsx
@@ -1,12 +1,19 @@
 import { useTranslation } from "react-i18next";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { Languages } from "../i18n";
+import { setUIFPS, uiFPS } from "../World/uiThrottler";
 
 export const Page = (): React.ReactElement => {
+  const [fps, setFPS] = useState(uiFPS);
   const { t, i18n } = useTranslation();
 
   const onLanguageChange = (ev: ChangeEvent<HTMLSelectElement>): void => {
     i18n.changeLanguage(ev.target.value);
+  };
+  const onFPSChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    const v = parseFloat(ev.target.value);
+    setUIFPS(v);
+    setFPS(v);
   };
   return (
     <>
@@ -18,6 +25,15 @@ export const Page = (): React.ReactElement => {
           </option>
         ))}
       </select>
+      <br />
+      <p>FPS: {fps}</p>
+      <input
+        type="range"
+        min="1"
+        max="100"
+        value={fps}
+        onChange={onFPSChange}
+      />
     </>
   );
 };
