40593eec8789238448593a1fe87f0633d6ecf788
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-03-19 22:05:57 -0400

    map parser creates a fully valid map

--- src/Game/defaultValues.ts
+++ src/Game/defaultValues.ts
@@ -1,8 +1,10 @@
 import { Entity, EntityType } from "../types/entity";
 import { ID } from "./id";
 import defaultMapData from "./defaultMap.txt?raw";
+import { Map, Offset, ValuesPerTile } from "../types/map";
+import { Tile } from "../types/tile";
 
-export const parseMap = (s: string): number[][][] => {
+export const parseMap = (s: string): Map => {
   const mapData: number[][][] = [];
   for (const line of s.split("\n")) {
     const row: number[][] = [];
@@ -12,7 +14,29 @@ export const parseMap = (s: string): number[][][] => {
     }
     mapData.push(row);
   }
-  return mapData;
+
+  const width = mapData[0].length;
+  const height = mapData.length;
+  const bounds = new Int32Array([0, 0, width, height]);
+  const data = new Int32Array(width * height * ValuesPerTile);
+  for (let j = 0; j < bounds[3]; j++) {
+    for (let i = 0; i < bounds[2]; i++) {
+      data[(j * width + i) * ValuesPerTile + Offset.TILE_ID] =
+        mapData[j][i][Offset.TILE_ID];
+
+      if (mapData[j][i][Offset.TILE_ID] === Tile.MANA_CRYSTAL) {
+        data[(j * width + i) * ValuesPerTile + Offset.DATA_0] =
+          mapData[j][i][Offset.DATA_0];
+        data[(j * width + i) * ValuesPerTile + Offset.DATA_1] =
+          mapData[j][i][Offset.DATA_1];
+      }
+    }
+  }
+
+  return {
+    bounds,
+    data,
+  };
 };
 
 export const defaultMap = parseMap(defaultMapData);
