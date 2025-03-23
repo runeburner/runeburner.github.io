d56c84b4a3ce8d3e5ec09386981268efe9cfc10f
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-03-23 17:25:18 -0400

    get rid of a bunch of as

--- src/Game/actionHandlers/attune.ts
+++ src/Game/actionHandlers/attune.ts
@@ -4,13 +4,14 @@ import {
   ATTUNE,
   ATTUNEProgress,
 } from "../../types/actions";
-import { Entity, EntityType, GolemEntity } from "../../types/entity";
+import { Entity, EntityType } from "../../types/entity";
 import { dist } from "../../types/vec";
 import { game } from "../game";
 
 const maker = (a: ATTUNE): ActionProgress | true | null => {
-  const old = game.actionM.get(a.id) as ActionProgress | undefined;
-  const golem = game.entityM.get(a.id) as GolemEntity;
+  const old = game.actionM.get(a.id);
+  const golem = game.entityM.get(a.id);
+  if (golem?.__type !== EntityType.GOLEM) return null;
   const heart = ((): Entity | undefined => {
     for (const e of game.entityM.values()) {
       if (e.__type === EntityType.HEART) return e;
@@ -37,10 +38,10 @@ const maker = (a: ATTUNE): ActionProgress | true | null => {
 };
 const processor = (
   rate: number,
-  e: Entity,
+  golem: Entity,
   action: ATTUNEProgress
 ): boolean => {
-  const golem = e as GolemEntity;
+  if (golem.__type !== EntityType.GOLEM) return true;
   action.progress[0] += golem.mineSpeed * rate * game.powers.attune_power;
   while (
     action.progress[0] >= action.progress[1] &&
