c19a7d772e93bc903da59c98d4db53327fb3800a
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-03-23 17:16:10 -0400

    var rename

--- src/Editor/Editor/api.d.ts
+++ src/Editor/Editor/api.d.ts
@@ -1,10 +1,12 @@
 // This file is ONLY used by the text editor. Not by the game itself.
 type Vec = [number, number];
 type RS = {
-  findNearest(e: Entity, tile: string, radius: number): Vec | null;
-  me(e: Entity): Entity;
-  isInRange(e: Entity, v: Vec): boolean;
-  findClosestEntity(e: Entity, entityType: string): Vec | null;
+  findNearest(tile: string, radius: number): Vec | null;
+  findAll(tile: string, radius: number): Vec[];
+  me(): unknown;
+  at(v: Vec): Int32Array;
+  isInRange(v: Vec): boolean;
+  findClosestEntity(entityType: string): unknown | null;
 };
 
 declare const MOVE_NEXT_TO: (v: Vec) => unknown;
