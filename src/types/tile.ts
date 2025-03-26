5627524dcdd2bcbed6b33e8a89f856e9c579763b
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-03-26 19:28:51 -0400

    improve challenge

--- src/types/tile.ts
+++ src/types/tile.ts
@@ -1,6 +1,7 @@
 export const Tile = Object.freeze({
   EMPTY: 0,
   RUNE_CRYSTAL: 1,
+  ROCK: 2,
 } as const);
 
 export type Tile = (typeof Tile)[keyof typeof Tile];
@@ -8,4 +9,5 @@ export type Tile = (typeof Tile)[keyof typeof Tile];
 export const EnterWeight: { [T in Tile]: number } = {
   [Tile.EMPTY]: 1,
   [Tile.RUNE_CRYSTAL]: Infinity,
+  [Tile.ROCK]: Infinity,
 };
