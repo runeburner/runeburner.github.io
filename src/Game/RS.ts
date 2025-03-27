ed0762ce26dbea63e0900f0e50fe26ce84dea532
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-03-27 12:10:39 -0400

    Refine API

--- src/Game/RS.ts
+++ src/Game/RS.ts
@@ -1,3 +1,4 @@
+import { ATTUNE, DIE, MINE, MOVE_NEXT_TO, SMASH } from "../types/actions";
 import { Entity, EntityType } from "../types/entity";
 import { Tile } from "../types/tile";
 import { dist } from "../types/vec";
@@ -5,27 +6,44 @@ import { game } from "./game";
 import { isArgs, isNumber, isString, isVec } from "./validation";
 
 export const rs = {
-  findNearest(e: Entity, tile: Tile, radius: number): Vec | null {
-    if (!isArgs([tile, radius], isNumber, isNumber)) return null;
-    return game.findClosestTile(e.pos, tile, radius);
+  game: {
+    findNearest(e: Entity, tile: Tile, radius: number): Vec | null {
+      if (!isArgs([tile, radius], isNumber, isNumber)) return null;
+      return game.findClosestTile(e.pos, tile, radius);
+    },
+    findAll(e: Entity, tile: Tile, radius: number): Vec[] {
+      if (!isArgs([tile, radius], isNumber, isNumber)) return [];
+      return game.findAllTiles(e.pos, tile, radius);
+    },
+    me(e: Entity): Entity {
+      return structuredClone(e);
+    },
+    at(_e: Entity, v: Vec): Int32Array {
+      if (!isArgs([v], isVec)) return new Int32Array();
+      return game.tileAt(v);
+    },
+    isInRange(e: Entity, v: Vec): boolean {
+      if (!isArgs([v], isVec)) return false;
+      return dist(e.pos, v) <= 1;
+    },
+    findClosestEntity(e: Entity, entityType: EntityType): Entity | null {
+      if (!isArgs([entityType], isString)) return null;
+      return game.findClosestEntity(e.pos, entityType);
+    },
   },
-  findAll(e: Entity, tile: Tile, radius: number): Vec[] {
-    if (!isArgs([tile, radius], isNumber, isNumber)) return [];
-    return game.findAllTiles(e.pos, tile, radius);
-  },
-  me(e: Entity): Entity {
-    return structuredClone(e);
-  },
-  at(_e: Entity, v: Vec): Int32Array {
-    if (!isArgs([v], isVec)) return new Int32Array();
-    return game.tileAt(v);
-  },
-  isInRange(e: Entity, v: Vec): boolean {
-    if (!isArgs([v], isVec)) return false;
-    return dist(e.pos, v) <= 1;
-  },
-  findClosestEntity(e: Entity, entityType: EntityType): Entity | null {
-    if (!isArgs([entityType], isString)) return null;
-    return game.findClosestEntity(e.pos, entityType);
+  act: {
+    MOVE_NEXT_TO: (e: Entity, v: Vec): MOVE_NEXT_TO => ({
+      __type: "MOVE_NEXT_TO",
+      id: e.id,
+      v: v,
+    }),
+    MINE: (e: Entity, v: Vec): MINE => ({ __type: "MINE", v: v, id: e.id }),
+    ATTUNE: (e: Entity): ATTUNE => ({ __type: "ATTUNE", id: e.id }),
+    DIE: (e: Entity): DIE => ({ __type: "DIE", id: e.id }),
+    SMASH: (e: Entity, id: number): SMASH => ({
+      __type: "SMASH",
+      id: e.id,
+      target: id,
+    }),
   },
 };
--- src/Game/workerScriptHeader.js
+++
@@ -1,5 +0,0 @@
-const MOVE_NEXT_TO = (v) => ({ __type: "MOVE_NEXT_TO", v: v, id: ENTITY_ID });
-const MINE = (v) => ({ __type: "MINE", v: v, id: ENTITY_ID });
-const ATTUNE = () => ({ __type: "ATTUNE", id: ENTITY_ID });
-const DIE = () => ({ __type: "DIE", id: ENTITY_ID });
-const SMASH = (id) => ({ __type: "SMASH", id: ENTITY_ID, target: id });
