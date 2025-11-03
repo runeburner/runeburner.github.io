4ccdb5f64b065b5273ea45eb333194fd200c657f
hydroflame <79246165+hydroflame@users.noreply.github.com>
2025-11-03 10:22:56 -0500

    Add notes and update donation counter

--- src/Game/formulas.ts
+++ src/Game/formulas.ts
@@ -2,6 +2,6 @@ export const leafPower = (n: number): number => {
   return 1 + 0.01 * n;
 };
 
-const donations = 213;
+const donations = 230;
 
 export const BloodRunePower = 1 + donations / 100 / 50;
